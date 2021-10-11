ca091c420286fe2289489085480730f1ddb96c97
amedeo <amedeopasanisi97@gmail.com>
2021-10-11 17:56:57 +0200

    creazione elementi html, css

--- index.php
+++ index.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
     <title>Google FAQ</title>
 </head>
-<body>
+<body class="container roboto">
     <?php
         $faqs = [
             [
@@ -29,6 +33,12 @@
                 ]
             ],
             [
+                "domanda"=> "Perché il mio account è associato a un paese?",
+                "paragrafiRisposta" => [
+                    
+                ]
+            ],
+            [
                 "domanda"=> "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
                 "paragrafiRisposta" => [
                     "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni."
@@ -41,24 +51,22 @@
                 ]
             ],
         ];
-
+    ?>
+    <?php
         foreach($faqs as $faq) {
+            echo "<div class='faq'>";
             foreach($faq as $key => $element) {
                 if ($key == "domanda") {
-                    echo "<strong>" . $key . " : " . "<br>" . "</strong>";
-                    echo $element . "<br>" . "<br>";
+                    echo "<h2>" . $element . "</h2>";
                 }
                 else {
-                    echo "<strong>" . $key . " : " . "<br>" . "</strong>";
                     foreach($element as $paragrafo) {
-                        echo $paragrafo . "<br>";
-                        echo "<br>";
+                        echo "<p>" . $paragrafo . "</p>";
                     }
                 }
             }
-            echo "<br>" . "<br>";
+            echo "</div>";
         }
     ?>
-
 </body>
 </html>
